49f8bf1fc57fac7bbcdfa43c8db2d279e562324d
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-06 19:50:17 -0500

    Frontend - Header & Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
-const App = () => {
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () =>{
   return (
-    <h1>App</h1>
+    <>
+      <Header/>
+      <main className="py-3"> {/* Main content area with padding on the y-axis */}
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer/>
+      
+    </>
+    
   )
 }
+
 export default App;
