89f9eb38b97f94afa35a54f45063ceb4aca260fd
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-09 17:10:52 -0500

    adding Alert message and Loader component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 
 const ProductScreen = () => {
@@ -16,9 +18,9 @@ const ProductScreen = () => {
             Go Back
             </Link>
             { isLoading ? (
-                <h2>Loading...</h2>
+                <Loader />
             ): error ? (
-                <div>{ error?.data?.message || error } </div>
+                <Message variant = "danger">{ error?.data?.message || error } </Message>
             ) : (
                 <Row>
                 <Col md = {5}>
