57b44cf62d77bf05ef58c58bfc1dfa9fd514bf98
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-11 13:23:10 -0500

    Adding Cart utils file

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,10 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: []};
 
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-}
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -22,23 +20,7 @@ const cartSlice = createSlice({
                 state.cartItems=[...state.cartItems, item]
             }
 
-            //Calculate items price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price *item.qty, 0));
-            
-            //Calculate Shipping Price (if Order is more than $100 then free, or else $10 Shipping)
-            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-            //Calculate tax price(15% tax)
-            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-
-            //Calculate Total price
-            state.totalPrice = (
-                Number(state.itemsPrice) +
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-            ).toFixed(2);
-
-            localStorage.setItem('cart', JSON.stringify(state));
+            return updateCart(state);
         }
     },
 });
