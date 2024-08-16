2ee6a9395f6abc9f969d8376e496241055622b5d
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-16 17:37:10 -0500

    Create review FrontEnd

--- frontend/src/screens/admins/UserEditScreen.jsx
+++ frontend/src/screens/admins/UserEditScreen.jsx
@@ -56,6 +56,7 @@ const UserEditScreen = () => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
         {isLoading ? (
           <Loader />
         ) : error ? (
