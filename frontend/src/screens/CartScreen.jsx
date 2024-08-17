ea17b24f7ddf14f9c440500a800a38907288a4b2
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-16 19:30:44 -0500

    Create Pagination controller and route

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -28,7 +28,7 @@ const CartScreen = () => {
     return (
         <Row>
             <Col md = {8}>
-                <h1 style={{ marginBottom: '20px' }}>Shoppint Cart</h1>
+                <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
                 {cartItems.length === 0 ? (
                     <Message>
                         Your Cart is Empty <Link to='/'>Go Back</Link>
