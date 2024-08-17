e4cf70d2d687bd9728e4236013071a09b0b5f306
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-17 02:58:54 -0500

    Added Pagination in Env and added Deploy script in Json root and built react project

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ ectended:true }))
 // cookie Parser Middlewate
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-    res.send("API is running...");
-});
-
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -38,8 +34,26 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();  // set __dirname to current Directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
