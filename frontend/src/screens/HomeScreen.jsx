867c0d79069930660b610109e3331daec57ce4e4
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-16 20:05:55 -0500

    Create Pagination frontend for homescreen and Product List Screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,14 +4,13 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
     
     const { pageNumber } = useParams();
 
     const { data, isLoading, error }= useGetProductsQuery({ pageNumber });
-
-    
 
     return (
         <>
@@ -28,6 +27,7 @@ const HomeScreen = () => {
                         </Col>
                     ))}
                 </Row>
+                <Paginate pages={data.pages} page={data.page} />
             </>)}
 
             
