93c6c2a168563f4705ae08b60425c55b53a6cbad
Sudheesh T D <sudheeshtd01@gmail.com>
2024-08-16 20:48:03 -0500

    Keyword and Search link added with pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,15 +4,22 @@ import Product from '../models/productModel.js';
 // @desc     Fetch all products
 // @route    GET /api/products
 // @access   Public
-const getProducts = asyncHandler(async(req, res) => {
-  const pageSize = 4;
-  const page = Number(req.query.pageNumber) || 1;
-  const count = await Product.countDocuments();
+const getProducts = asyncHandler( async (req, res) =>{
 
-  const products = await Product.find({})
-  .limit(pageSize)
-  .skip(pageSize * (page -1));
-  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+  const pageSize = 8;
+  const page = Number(req.query.pageNumber) || 1;
+  
+
+  const keyword = req.query.keyword ? { name: { $regex: req.query.keyword, $options: 'i'}}
+  : {};
+
+  const count = await Product.countDocuments({...keyword});
+
+  const products = await Product.find({...keyword})
+      .limit(pageSize)
+      .skip(pageSize * (page - 1));
+  res.json({products, page, pages: Math.ceil(count/pageSize)});
+
 });
 
 
